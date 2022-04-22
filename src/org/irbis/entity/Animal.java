23c1353becbebb09c7cb79e07989e5d8a6f773e9
Yaroslav Ryabov <yar.ryabov@mail.ru>
2022-04-22 06:31:17 +0400

    add foods field in Animal

--- src/org/irbis/entity/Animal.java
+++ src/org/irbis/entity/Animal.java
@@ -5,14 +5,16 @@ public class Animal {
     private String name;
     private String type;
     private int age;
+    private String[] foods;
     // write your fields here if needed
 
 
-    public Animal(String country, String name, String type, int age) {
+    public Animal(String country, String name, String type, int age, String[] foods) {
         this.country = country;
         this.name = name;
         this.type = type;
         this.age = age;
+        this.foods = foods;
     }
 
     public String getCountry() {
@@ -29,5 +31,9 @@ public class Animal {
 
     public int getAge() {
         return age;
+    }
+
+    public String[] getFoods() {
+        return foods;
     }
 }
